376aa7ad52d8e00321580dd7b1850c86392cdb89
Sergey Kozlov <mail.sergey.kozlov@gmail.com>
2016-10-24 22:22:58 +0700

    Dialog.open

---
+++ bacon.jquery.ui.js
@@ -0,0 +1,38 @@
+(function(){
+	var Bacon = window.Bacon;
+	if(!Bacon){
+		Bacon = {};
+		window.Bacon = Bacon;
+	}
+	if(!Bacon.$){
+		Bacon.$ = {};
+	}
+	Bacon.$.UI = new function(){
+		this.Dialog = function(jqObject, options){
+			jqObject.dialog(options);
+
+			function isOpen(){
+				return jqObject.dialog('isOpen');
+			}
+
+			this.open = Bacon.Binding({
+				initValue: isOpen(),
+				get: isOpen(),
+				set: function(open){
+					if(open ^ isOpen()) {
+						if (open) {
+							jqObject.dialog('open');
+						} else {
+							jqObject.dialog('close');
+						}
+					}
+				},
+				events: Bacon.never()
+			});
+
+			this.open.addSource(
+				jqObject.asEventStream('dialogclose').map(false)
+					.merge(jqObject.asEventStream('dialogopen').map(true)));
+		};
+	}();
+})();
